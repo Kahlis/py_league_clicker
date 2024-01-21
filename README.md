5a2b72918a0af8bcaaf1a50191793ec41c0f87a2
Matheus Kahlis <kahlis.dev@gmail.com>
2024-01-20 23:39:35 -0300

    feat: Click to button added

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # py_league_clicker
+
+Ubuntu
+    You need to install gnome-screenshot or will get the error:
+    Exception: To take screenshots, you must install Pillow version 9.2.0 or greater and gnome-screenshot by running `sudo apt install gnome-screenshot`
--- __main__.py
+++ __main__.py
@@ -1,7 +1,14 @@
+import os.path
+
 import pyautogui
 import time
 
-time.sleep(1)
-location = pyautogui.locateOnScreen('assets/accept.png')
-pyautogui.moveTo(location)
-pyautogui.click()
+if os.path.exists('/home/kahlis/Documentos/Projects/python/py_league_clicker/assets/accept.png'):
+    print('Correct path')
+
+try:
+    location = pyautogui.locateOnScreen('assets/accept.png', confidence=0.75)
+    pyautogui.moveTo(location)
+    pyautogui.click()
+except pyautogui.ImageNotFoundException as e:
+    print(f'Match not found')
