db3ab3822bb6f84d0b670ce3d9f33594aebeeb5a
Matheus Kahlis <kahlis.dev@gmail.com>
2024-01-21 00:03:27 -0300

    feat: Click functionality complete

--- __main__.py
+++ __main__.py
@@ -1,16 +1,17 @@
 import os.path
-
 import pyautogui
 import time
-
 
 absolute_path = os.path.abspath('assets/accept.png')
 if os.path.exists(absolute_path):
     print('Assets OK')
 
-try:
-    location = pyautogui.locateOnScreen('assets/accept.png', confidence=0.75)
-    pyautogui.moveTo(location)
-    pyautogui.click()
-except pyautogui.ImageNotFoundException as e:
-    print(f'Match not found')
+while True:
+    try:
+        location = pyautogui.locateOnScreen('assets/accept.png', confidence=0.75)
+        pyautogui.moveTo(location)
+        pyautogui.click()
+        print("Match found")
+        break
+    except pyautogui.ImageNotFoundException as e:
+        time.sleep(3)
