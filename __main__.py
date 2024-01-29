3b0b7a38614a4133a0ad2ef36b43fd361a0708ec
Matheus Kahlis <kahlis.dev@gmail.com>
2024-01-28 22:46:18 -0300

    feat: Added deny option

--- __main__.py
+++ __main__.py
@@ -3,15 +3,19 @@ import pyautogui
 import time
 
 absolute_path = os.path.abspath('assets/accept.png')
+accept = True
+accept_png = 'assets/accept.png'
+deny_png = 'assets/deny.png'
+
 if os.path.exists(absolute_path):
     print('Assets OK')
 
 while True:
     try:
-        location = pyautogui.locateOnScreen('assets/accept.png', confidence=0.75)
+        location = pyautogui.locateOnScreen(accept_png if accept else deny_png, confidence=0.75)
         pyautogui.moveTo(location)
         pyautogui.click()
-        print("Match found")
+        print("Match found.")
         break
     except pyautogui.ImageNotFoundException as e:
         time.sleep(3)
